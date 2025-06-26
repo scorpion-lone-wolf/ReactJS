3b2ab00c7c95333ac6fd80fa1b07dfc835c1486d
CODECLOUDS-PURSOTAM <codeclouds-purosattam@CISPL-IO-751.local>
2025-06-26 20:20:31 +0530

    added toggle button

--- Projects/components/src/components/Pricetable.jsx
+++ Projects/components/src/components/Pricetable.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import "./pricetable.css";
 
 const Pricetable = () => {
+  const [isOn, setIsOn] = useState(false);
   return (
     <div className="pricetable">
       <div className="pricetable__top">
         <h1>Our Pricing</h1>
         <div className="pricetable__season">
           <span>Anually</span>
-          <span>Toogle</span>
+          <label className="switch">
+            <input
+              type="checkbox"
+              id="toggle"
+              checked={isOn}
+              onChange={() => {
+                console.log("The toogle is ", isOn);
+                setIsOn(!isOn);
+              }}
+            />
+            <span className="slider"></span>
+          </label>
           <span>Monthly</span>
         </div>
       </div>
       <div className="pricetable__box">
-        <div className="pricetable__standard">Standard</div>
-        <div className="pricetable___professional">Professional</div>
-        <div className="pricetable__master">Master</div>
+        <div className="pricetable__standard"></div>
+        <div className="pricetable___professional"></div>
+        <div className="pricetable__master"></div>
       </div>
     </div>
   );
