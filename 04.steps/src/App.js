f19e4b35e846564aba615621e442aba454b536c3
CODECLOUDS-PURSOTAM <codeclouds-purosattam@CISPL-IO-751.local>
2025-06-29 11:55:19 +0530

    added open and close button for steps

--- 04.steps/src/App.js
+++ 04.steps/src/App.js
@@ -2,37 +2,45 @@ import { useState } from "react";
 import "./app.css";
 function App() {
   const [currentActive, setCurrentActive] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   const messages = ["Learn React", "Build Projects", "Deploy your Projects"];
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`one ${currentActive >= 1 ? "active" : ""}`}>1</div>
-        <div className={`two ${currentActive >= 2 ? "active" : ""}`}>2</div>
-        <div className={`three ${currentActive >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="messages">
-        Step {currentActive} : {messages[currentActive - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          onClick={() => {
-            if (currentActive !== 1) {
-              setCurrentActive(prevValue => prevValue - 1);
-            }
-          }}
-        >
-          Previous
-        </button>
-        <button
-          onClick={() => {
-            if (currentActive !== 3) {
-              setCurrentActive(prevValue => prevValue + 1);
-            }
-          }}
-        >
-          Next
-        </button>
-      </div>
+    <div className="container">
+      <button className="close" onClick={() => setIsOpen(prev => !prev)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`one ${currentActive >= 1 ? "active" : ""}`}>1</div>
+            <div className={`two ${currentActive >= 2 ? "active" : ""}`}>2</div>
+            <div className={`three ${currentActive >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="messages">
+            Step {currentActive} : {messages[currentActive - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={() => {
+                if (currentActive !== 1) {
+                  setCurrentActive(prevValue => prevValue - 1);
+                }
+              }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => {
+                if (currentActive !== 3) {
+                  setCurrentActive(prevValue => prevValue + 1);
+                }
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
