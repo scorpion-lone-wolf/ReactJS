7a215c3e9221b4ea7a9583ce50643f48520fed0a
CODECLOUDS-PURSOTAM <codeclouds-purosattam@CISPL-IO-751.local>
2025-09-13 15:25:03 +0530

    added clear cart

--- 14.fast-react-pizza/src/features/cart/Cart.jsx
+++ 14.fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,36 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
 import CartItem from "./CartItem";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { clearCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 function Cart() {
-  const cart = fakeCart;
   const userName = useSelector((state) => state.user.userName);
+  const cart = useSelector((state) => state.cart.cart);
+  const dispatch = useDispatch();
 
+  function handleClearCart() {
+    dispatch(clearCart());
+  }
+
+  if (cart.length <= 0) {
+    return <EmptyCart />;
+  }
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -47,7 +33,7 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary" className="">
+        <Button type="secondary" onClick={handleClearCart}>
           Clear cart
         </Button>
       </div>
